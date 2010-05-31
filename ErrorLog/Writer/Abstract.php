9c4fe4f7cf1648cc81dfec2adb29b4385fe5b59d
Ludovic Bellière <xrogaan@gmail.com>
2010-06-01 00:33:17 +0200

    Update file: Abstract writer
    Default value of the extra parameter in the store method is set to null
    Extra data is now processed and added to the logData.
    
    Expand StoreException and StoreMessage

--- ErrorLog/Writer/Abstract.php
+++ ErrorLog/Writer/Abstract.php
@@ -11,13 +11,18 @@ abstract class Errorlog_Writer_Abstract {
     /**
      * Open a log ressource.
      */
-    abstract public function open() {}
+    abstract public function open();
     
     /**
      * Close a log ressource.
      */
-    abstract public function close() {}
-    
+    abstract public function close();
+
+    /**
+     * Write a message to the log.
+     */
+    abstract protected function _write();
+
     /**
      * Store error data into the log
      *
@@ -27,24 +32,20 @@ abstract class Errorlog_Writer_Abstract {
      * @param array   $errno (optionnal)
      * @param mixed   $extra (optionnal)
      */
-    public function store($message, $severity, $file='', $line=0, $trace=array(), $extra=array())
+    public function store($message, $severity, $file='', $line=0, $trace=array(), $extra=null)
     {    
         if (empty($trace))
         {
             $trace = null;
         }
-        
-        if (empty($extra))
-        {
-            $extra = null;
-        }
+
         
         if (empty($file) && !$line)
         {
             $this->storeMessage($message, $severity);
         }
         
-        return array(
+       $logData = array(
             'message' => $message,
             'file'    => $file,
             'line'    => $line,
@@ -56,11 +57,53 @@ abstract class Errorlog_Writer_Abstract {
                 'cookie'  => $_COOKIE
             ),
             'env'     => $_SERVER,
-            'extra'   => $extra
         );
+
+        if (!empty($extra)) {
+            if (is_array($extra)) {
+                $info = array();
+                foreach ($extra as $key => $value) {
+                    if (is_string($key)) {
+                        $logData[$key] = $value;
+                    } else {
+                        $info[] = $value;
+                    }
+                }
+            } else {
+                $info = $extra;
+            }
+
+            if (!empty($info)) {
+                $logData['info'] = $info;
+            }
+        }
     }
     
-    protected function storeMessage($message, $severity) {}
-    protected function storeException($errstr, $code, $errfile, $errline, $traceback, $severity=0) {}
+    protected function storeMessage($message, $severity) {
+        return array(
+            'message'  => $message,
+            'severity' => $severity
+        );
+    }
+
+    protected function storeException($errstr, $code, $errfile, $errline, $traceback, $severity=0) {
+
+        if (is_null($severity) && $code !== false) {
+            $severity = $code;
+        }
+
+        if (empty($traceback)) {
+            $traceback = debug_backtrace();
+        }
+
+        return array(
+            'message'   => $errstr,
+            'severity'  => $severity,
+            'file'      => $errfile,
+            'line'      => $errline,
+            'traceback' => $traceback,
+            'code'      => $code
+        );
+    }
 }
 
